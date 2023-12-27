8c85933ab7952c4cb983b159e42baf4c8da4ebd0
Aidan Feil <aidanfeil99@gmail.com>
2023-12-27 14:22:06 +1100

    Added Features and modified README
    Added two functions in naughts_and_crosses.py to display the current
    board at any time, and an example layout for the introduction.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # naughts-and-crosses
 Naughts and Crosses (CMD) Python Project
 
-27/12/2023 | Project Started
-           |This is a CMD based naughts and crosses game for demonstrating user input, nested arrays, and condition checks.
+27/12/2023  |v0.0.0 |Project Started
+            |v0.1.0 |This is a CMD based naughts and crosses game for demonstrating user input, nested arrays, and condition checks.
+            |v0.1.1 |Added functions to display the board layout and the current state of the board. Added extra lines to the introduction. 
--- naughts_and_crosses.py
+++ naughts_and_crosses.py
@@ -1,11 +1,31 @@
 import os
 
+game_board = [[" "," "," "],[" "," "," "],[" "," "," "]]
+
+# Function to print the current state of the board
+def print_board():
+    print(" {} | {} | {} ".format(game_board[0][0],game_board[0][1],game_board[0][2]))
+    print(" {} | {} | {} ".format(game_board[1][0],game_board[1][1],game_board[1][2]))
+    print(" {} | {} | {} ".format(game_board[2][0],game_board[2][1],game_board[2][2]))
+
+
 # Function that prints the introduction page on code launch, will flow to an example of the win condition and board/input layout.
 def print_intro():
     os.system('cls')
-    print("Welcome To Naughts and Crosses!\n\n")
+    print("Welcome To Naughts and Crosses!\n")
     print("The rules are as follows:")
     print("    1. The Naught player will always start the game.")
-    print("    2. The game ends after one player obtains three of their markers in a row.")
+    print("    2. The game ends after one player is able to place three of their markers in a row, or if all places on the board are filled.")
+    input("Please press enter/return to continue:")
+    print("\nThe board layout is as follows:\n")
+    print_layout()
+    print("\nPlayers will select the number that corresponds with the location on the board they wish to place their marker.")
+
+
+# Function to print the layout of the board. What number each space is mapped to.
+def print_layout():
+    print(" {} | {} | {} ".format(1,2,3))
+    print(" {} | {} | {} ".format(4,5,6))
+    print(" {} | {} | {} ".format(7,8,9))
 
 print_intro()
